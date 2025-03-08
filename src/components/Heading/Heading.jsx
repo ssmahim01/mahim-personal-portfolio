8c1cf9ec4235dba405eaecd461288a9bcf5f977f
ssmahim01 <mahimzayn666@gmail.com>
2025-03-09 00:04:40 +0600

    Decrease margin from bottom in Heading component

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,6 +1,6 @@
 const Heading = ({title, subHeading}) => {
     return (
-        <div className="mb-6">
+        <div className="mb-4">
             <h2 className="text-center md:text-4xl text-3xl font-extrabold">{title}</h2>
 
             <p className="text-center mt-2 text-gray-500 font-semibold">{subHeading}</p>
